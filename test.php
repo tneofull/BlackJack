ce39f25a848d2654fa0c0f81301d8e82df5e126a
tneofull <103440576+tneofull@users.noreply.github.com>
2022-09-06 18:11:07 +0900

    ブラックジャックゲーム初期の設定がある程度完了

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hello" . PHP_EOL;
+
+foreach ([1, 2, 3] as $val) {
+    echo $val . PHP_EOL;
+}
+
+
+
+class Foo
+{
+    public function bar()
+    {
+        $sh = 'short test';
+        $longlonglonglongolonglong = 'long test';
+    }
+}
